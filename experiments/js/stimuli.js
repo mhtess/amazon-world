ce48bde64e0f3445296119a7552e162b598e6dad
MH Tessler <mht137@gmail.com>
2019-06-13 10:23:54 -0400

    expt files, first draft

---
+++ experiments/js/stimuli.js
@@ -0,0 +1,16 @@
+var single_box_stimuli = [
+  {
+    num_objects: 3,
+    contents_information: {type: ["books", 2]},
+    box_information: {price: "$20"},
+    query: "price",
+    question: "What is the price of the 3rd object?"
+  },
+  {
+    num_objects: 3,
+    contents_information: {type: ["books", 2]},
+    box_information: {price: "$20"},
+    query: "weight",
+    question: "How much does the box weigh?"
+  }
+]
